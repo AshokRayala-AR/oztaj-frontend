48d0a3453e45e38d12266135ff7e646ca9a4d3d3
AshokRayala-AR <rayalaashok75@gmail.com>
2023-02-28 17:13:15 +0530

    completed the basic version

---
+++ mahender.js
@@ -0,0 +1,4 @@
+const firstQuote = document.getElementById('first-quote');
+const secondQuote = document.getElementById('second-quote');
+const thirdQuote = document.getElementById('third-quote');
+
