c40106e666be570476bdee93f782da31a3cabce3
AshokRayala-AR <rayalaashok75@gmail.com>
2023-02-28 17:23:31 +0530

    rename files, and delete unnesessary files

--- mahender.js
+++
@@ -1,4 +0,0 @@
-const firstQuote = document.getElementById('first-quote');
-const secondQuote = document.getElementById('second-quote');
-const thirdQuote = document.getElementById('third-quote');
-
