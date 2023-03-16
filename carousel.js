c85e129aa98a9099fc1af5b81b091043d33e8d11
AshokRayala-AR <rayalaashok75@gmail.com>
2023-03-16 12:28:13 +0530

    progressive image loading added

--- carousel.js
+++ carousel.js
@@ -1,3 +1,5 @@
+// quotation scrolling script
+
 var quotation = document.getElementById("quote-para");
 const quotes = [
   "We fuel your growth",
@@ -13,3 +15,30 @@ var typewriter = new Typewriter(quotation, {
   autoStart: true,
   pauseFor: 3000,
 });
+
+// progressive image loading script
+
+const images = document.querySelectorAll("[src]");
+function preloadImage(img) {
+  const source = img.getAttribute("src");
+  if (!source) {
+    return;
+  }
+  img.src = source;
+}
+const imgOptions = {};
+
+const ImgObserver = new IntersectionObserver((entries, imgObserver) => {
+  entries.forEach(entry => {
+    if(!entry.isIntersecting) {
+      return;
+    }else {
+      preloadImage(entry.target);
+      imgObserver.unobserve(entry.target);
+    }
+  })
+}, imgOptions);
+
+images.forEach(image => {
+  imgObserver.observe(image);
+})
